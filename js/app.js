030d5caef621f1f8a6c466935f2010232da93d7b
Tommi Grohn <grohn.tommi@gmail.com>
2015-12-16 10:17:02 +0200

    Some refactoring

--- js/app.js
+++ js/app.js
@@ -2,65 +2,30 @@
 
 angular
 .module('rotateAndMatch', ['element', 'mainGrid'])
-.controller('gameController', ['$scope', 'GridService', function($scope, GridService) { 
-	this.title = 'rotate-and-match';
+.controller('gameController', function(GridService) { 
 	this.elementSize = 100;
 	this.size = 5;
+
 	this.elements = GridService.createGrid(this.size, this.elementSize);
-	this.width = GridService.getWidth();
 
-	$scope.getBorders = function() {
-		return 'width:' + (controller.elementSize + 'px') + ','
-			+ 'height:' + (controller.elementSize + 'px') + ',';
+	this.getWidth = function() { 
+		return GridService.getWidth(); 
+	};
+	
+	this.getBorders = function() { 
+		return GridService.getBorders(); 
+	};
+	
+	this.rotate = function() { 
+		GridService.rotate(); 
 	};
 
-	$scope.rotate = function() {
-		GridService.grid
-			.map(function(element, index) {
-				var cpy = jQuery.extend({}, element);
-				return {content: cpy, i: index};
-			})
-			.filter(function(element) { 
-				return element.content.selected; 
-			})
-			.forEach(function(element, index) {
-				var s = GridService.size;
-				var newIndex = function(ind) {
-					switch (ind) {
-						case 0:
-							return element.i + 1;
-						case 1:
-							return element.i + s;
-						case 2:
-							return element.i - s;
-						case 3:
-							return element.i - 1;
-					};
-				};
-				GridService.grid[newIndex(index)].value = element.content.value;
-			});
+	this.toggleSelect = function(i) { 
+		return GridService.toggleSelect(i); 
 	};
-
-	$scope.toggleSelect = function(index) {
-		var s = GridService.size;
-		if (index % s < s - 1 && index < Math.pow(s, 2) - s) {
-			for (var i = 0; i < 4; i++) {
-				var m = i > 1 ? s - 2 : 0;
-				GridService.grid[index + i + m].selected ^= true;
-			}
-		}
+	
+	this.getColor = function(element) {
+		return GridService.getColor(element); 
 	};
-
-	$scope.getColor = function(element) {
-		var highlight = element.selected ? 64 : 0;
-		return 'rgb(' 
-			+ (highlight + element.value * 64 % 192) 
-			+ ',' 
-			+ (highlight + element.value * 8 % 196) 
-			+ ',' 
-			+ (highlight + element.value * 48 % 196) 
-			+ ')';
-	};
-
-}]);
+});
 
--- js/grid/mainGrid.js
+++ js/grid/mainGrid.js
@@ -4,22 +4,78 @@ angular
 	this.size = 0;
 	this.elementSize = 10;
 	this.grid = [];
+	var _this = this;
 	
 	this.createGrid = function(size, elementSize) {
-		this.size = size;
-		this.elementSize = elementSize;
-		this.grid = [];
+		_this.size = size;
+		_this.elementSize = elementSize;
+		_this.grid = [];
 		for (var i = 1; i <= Math.pow(size, 2); i++) {
 			var element = {
 				value: i,
 				selected: false
 			};
-			this.grid.push(element);
+			_this.grid.push(element);
 		}
-		return this.grid;
+		return _this.grid;
 	};
 
 	this.getWidth = function() {
-		return this.size * this.elementSize;
+		return _this.size * _this.elementSize;
+	};
+
+	this.getBorders = function() {
+		return 'width:' + (_this.elementSize + 'px') + ','
+			+ 'height:' + (_this.elementSize + 'px') + ',';
+	};
+
+	this.rotate = function() {
+		_this.grid
+			.map(function(element, index) {
+				return {value: element.value, selected: element.selected, i: index};
+			})
+			.filter(function(element) { 
+				return element.selected; 
+			})
+			.forEach(function(element, index) {
+				var s = _this.size;
+				var newIndex = function(ind) {
+					switch (ind) {
+						case 0:
+							return element.i + 1;
+						case 1:
+							return element.i + s;
+						case 2:
+							return element.i - s;
+						case 3:
+							return element.i - 1;
+						default:
+							return 0;
+					};
+				};
+				console.log(_this.size, newIndex(index));
+				_this.grid[newIndex(index)].value = element.value;
+			});
+	};
+
+	this.toggleSelect = function(index) {
+		var s = _this.size;
+		if (index % s < s - 1 && index < Math.pow(s, 2) - s) {
+			for (var i = 0; i < 4; i++) {
+				var m = i > 1 ? s - 2 : 0;
+				_this.grid[index + i + m].selected ^= true;
+			}
+		}
+	};
+
+	this.getColor = function(element) {	
+		var highlight = element.selected ? 64 : 0;
+		return 'rgb(' 
+			+ (highlight + element.value * 64 % 192) 
+			+ ',' 
+			+ (highlight + element.value * 8 % 196) 
+			+ ',' 
+			+ (highlight + element.value * 48 % 196) 
+			+ ')';
 	};
 });
